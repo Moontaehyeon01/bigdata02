7cb8643fb008e3ccb5fbd327030a6fbf872b0ab8
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-09-11 15:11:24 +0900

    v0.3 pandas

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -6,8 +6,9 @@ df4 = pd.DataFrame({
     '날짜': ['2025-09-11', '2025-09-11', '2025-09-12', '2025-09-12'],
     '도시': ['서울', '안양', '서울', '안양'],
     '온도': [23, 22, 24, 26]
-}, index=[1, 2, 3, 4])  # index는 별도 매개변수
+}, index=[1, 2, 3, 4])
 
 print(df4)
-df5 = df4.pivot(index='날짜', columns='도시', values='온도')
-print(df5)
+# df6 = df4.sort_values('온도') #ascending
+df6 = df4.sort_values('온도', ascending=False) # descendig
+print(df6)
