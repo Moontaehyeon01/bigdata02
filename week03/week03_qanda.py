98964c3ab70414d268161b993aa7dbb8e80e9383
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-09-11 16:23:13 +0900

    v0.1 qanda

---
+++ week03/week03_qanda.py
@@ -0,0 +1,16 @@
+# shallow copy, deep copy
+import copy
+
+a = [1, [3, -9], 4]
+# a[1][0] = 55
+
+print(a)
+b = a
+c = a.copy()
+d = a[:]
+e = copy.deepcopy(a)
+b[1][0]=55
+print(b)
+print(c)
+print(d)
+print(e)
