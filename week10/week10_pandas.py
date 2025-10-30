f56b65f8311a6a4b881fc7066976e51a5bf897d0
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-10-30 15:17:13 +0900

    v2.2 pandas

--- week10/week10_pandas.py
+++ week10/week10_pandas.py
@@ -8,4 +8,9 @@ german = pd.read_csv('german.csv')
 # print(german.tail())
 # print(german.head())
 # print(german['Age'].describe())
-print(pd.cut(german['Age'], bins=8).reset_index().groupby('Age').count())
+# print(pd.cut(german['Age'], bins=8).reset_index().groupby('Age').count())
+
+ages = [10, 20, 30, 40, 50, 60, 70, 80]
+# print(pd.cut(german['Age'], bins=ages).reset_index().groupby('Age').count())
+print(pd.cut(german['Age'], bins=ages, right=False).reset_index().groupby('Age').count())
+# print(german[german['Age']< 20])
