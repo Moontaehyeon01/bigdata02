01e45eec5fa9bb54879968d5cfbbb5cc3017ce9b
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-06 15:39:32 +0900

    v0.7 visualization

--- week11/week11_sklearn.py
+++ week11/week11_sklearn.py
@@ -1,10 +1,8 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 from sklearn.datasets import load_diabetes
-
-# 당뇨병 데이터셋 로드
 
 # 나이, 성별, 체질량지수, 평균혈압, TC 총 콜레스테롤(s1)
 # LDL 나쁜 콜레스테롤 수치(s2), HDL 좋은 콜레스테롤 수치(s3)
@@ -16,23 +14,31 @@ df_diabetes['target'] = diabetes.target  # 당뇨병 진행 정도 값
 
 fig, axes = plt.subplots(2, 2, figsize=(12, 8))
 
-axes[0, 0].hist(df_diabetes['s1'], bins=20)
-axes[0, 0].set_title("Diabetes Data : TC Distribution")
-axes[0, 0].set_xlabel("TC")
-axes[0, 0].set_ylabel("Frequency")
+axes[0, 0].hist(df_diabetes['s1'], bins=20, color='skyblue', edgecolor='red')
+axes[0, 0].set_title('Diabetes Data : TC Distribution')
+axes[0, 0].set_xlabel('TC')
+axes[0, 0].set_ylabel('Frequency')
 
-axes[0, 1].boxplot([df_diabetes['s1'],df_diabetes['s2'],df_diabetes['s3']])
+axes[0, 1].boxplot([df_diabetes['s1'], df_diabetes['s2'], df_diabetes['s3']])
 axes[0, 1].set_title('Diabetes Data : Boxplot')
-axes[0, 1].set_xticklabels(['TC','LDL','HDL'])
+axes[0, 1].set_xticklabels(['TC', 'LDL', 'HDL'])
 
 axes[1, 0].scatter(df_diabetes['s1'], df_diabetes['target'], color='coral', alpha=0.5)
 axes[1, 0].set_title('Diabetes Data : TC vs Progression')
-axes[1, 0].set_xlabel("TC")
-axes[1, 0].set_ylabel("Progression")
+axes[1, 0].set_xlabel('TC')
+axes[1, 0].set_ylabel('Progression')
 
-axes[1, 1].scatter(df_diabetes['bmi'], df_diabetes['target'], color='green', alpha=0.5)
+axes[1, 1].scatter(x=df_diabetes['bmi'], y=df_diabetes['target'], color='green', alpha=0.5)
 axes[1, 1].set_title('Diabetes Data : BMI vs Progression')
-axes[1, 1].set_xlabel("BMI")
-axes[1, 1].set_ylabel("Progression")
+axes[1, 1].set_xlabel('BMI')
+axes[1, 1].set_ylabel('Progression')
+
+plt.show()
+
+plt.figure(figsize=(12, 8))
+sns.histplot(df_diabetes['target'], color='blue', bins=10, kde=True)
+plt.title('Histogram of Diabetes Progression (Target)')
+plt.xlabel('Diabetes Progression')
+plt.ylabel('Frequency')
 
 plt.show()
