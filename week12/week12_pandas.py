fe3497114f828443e870898437a23c5ff8cf274d
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-13 14:52:01 +0900

    v1.1 visualization

--- week12/week12_pandas.py
+++ week12/week12_pandas.py
@@ -8,14 +8,20 @@ wh = pd.read_csv('wh.csv')
 # print(new_wh.info())
 # print(new_wh.describe())
 
-criteria = wh['Weight'].quantile(0.75) # 99.99% 이상인 데이터 지정
+criteria = wh['Weight'].quantile(0.9999) # 99.99% 이상인 데이터 지정
 # print(criteria) # 약 255.9 파운드
 # print(round(criteria, 1)) # 255.9
 new_wh = wh[wh['Weight'] < criteria]
 # print(new_wh.info())
 # print(new_wh.describe())
-sns.scatterplot(x='Weight', y='Height', data=new_wh)
-plt.show()
+# sns.scatterplot(x='Weight', y='Height', data=new_wh)
+# plt.show()
 
 # print(wh[wh['Weight'] > criteria])
 # print(wh[wh['Weight'] > criteria].index)
+
+wh['Weight'] = new_wh['Weight'].clip(100, 200) # lower bound, upper bound
+# print(wh.tail())
+# print(wh.info())
+sns.scatterplot(x='Weight', y='Height', data=wh)
+plt.show()
