a950b2fac7b072e9d61bdcd90ce90e914d912dc6
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-20 14:44:04 +0900

    v3.5 pandas

--- week13/week13_pandas.py
+++ week13/week13_pandas.py
@@ -6,12 +6,18 @@ apple = pd.read_csv('APPL_price.csv')
 # print(apple.head())
 # print(apple.describe())
 # print(apple.info())
-apple['Date']=pd.to_datetime(apple['Date']) # object -> datetime
+apple['Date'] = pd.to_datetime(apple['Date'])  # object -> datetime
 # print(apple.info())
-apple = apple.set_index('Date') # index를 날짜 시간 칼럼으로 대체
+apple = apple.set_index('Date')  # index를 날짜시간 칼럼으로 대체
 # print(apple.head())
-print(apple.tail())
+# print(apple.tail())
 
 # 시계열 데이터 슬라이싱
 # print(apple['2022-06-14':'2022-06-16'])
-print(apple['2022-04':'2022-06'])
+# print(apple['2022-04':'2022-06'])
+
+print(apple['Close'])
+apple_1months = apple.resample('1M').mean()  # 한달 단위로 행을 묶어 평균값 추출
+print(apple_1months['Close'])
+# print(apple.info())
+# print(apple_7days.describe())
